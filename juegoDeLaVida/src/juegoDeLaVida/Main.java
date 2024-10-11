a8e7fd5d19d829bf44f80ac67278637ba31441d0
unai <unai.carbajo.amundarain@gmail.com>
2024-10-11 09:13:26 +0200

    Programacion 11/10

--- juegoDeLaVida/src/juegoDeLaVida/Main.java
+++ juegoDeLaVida/src/juegoDeLaVida/Main.java
@@ -4,52 +4,48 @@ public class Main {
 
 	public static void main(String[] args) {
 		int[][] tabla = { { 0, 0, 0, 0, 0, 0 }, 
-					      { 0, 1, 1, 0, 0, 0, }, 
+					      { 0, 1, 1, 0, 0, 0,}, 
 					      { 0, 0, 1, 1, 0, 0 }, 
 					      { 0, 0, 1, 0, 0, 0 },
 					      { 0, 0, 0, 0, 0, 0 }, 
 					      { 0, 0, 0, 0, 0, 0 } };
 
-		int[][] aux = new int[3][3];
+		int[][] aux = new int[tabla.length][tabla.length];
+		
 		for (int i = 0; i < tabla.length; i++) {
 			for (int j = 0; j < tabla.length; j++) {
-				if (tabla[i][j] == 0)
-					System.out.print(". ");
-				else
-					System.out.print("* ");
+				System.out.print(tabla[i][j] + " ");
 			}
 			System.out.println();
 		}
 		
-		for (int i = 0; i < tabla.length; i++) {
-			for (int j = 0; j < tabla.length; j++) {
+		for (int fila = 0; fila < tabla.length; fila++) {
+			for (int columna = 0; columna < tabla.length; columna++) {
 				int suma = 0;
-				System.out.println(aux);
-				for (int k = -1; k <= 1; k++) {
-					for (int l = -1; l <= 1; l++) {
-						if ((i+k>=0 && i+k<6) && (j+l>=0 && j+l<6))
-	                        suma += tabla[i + k][j + l];
+				for (int filaI = -1; filaI <= 1; filaI++) {
+					for (int columnaJ = -1; columnaJ <= 1; columnaJ++) {
+						if ((fila+filaI>=0 && fila+filaI<6) && (columna+columnaJ>=0 && columna+columnaJ<6))
+	                        suma += tabla[fila + filaI][columna + columnaJ];
 
 					}
-					suma -= tabla[i][j];
+					suma -= tabla[fila][columna];
 				}
-				if (suma < 2 && suma > 3) {
-					tabla[i][j] = 0;
-				} else if (suma == 2 || suma <= 3) {
-					tabla[i][j] = 1;
+				if ((tabla[fila][columna] == 1 || tabla[fila][columna] == 0 ) && (suma < 2 || suma > 3)) {
+					aux[fila][columna] = 0;
+				}else if ((tabla[fila][columna] == 1) && (suma == 2 || suma == 3)) {
+					aux[fila][columna] = 1;
+				}else if (suma == 3)
+					aux[fila][columna] = 1; {
+					
+				} 
 				}
 			}
-		}
-		for (int i = 0; i < tabla.length; i++) {
-			for (int j = 0; j < tabla.length; j++) {
-				if (tabla[i][j] == 0)
-					System.out.print(". ");
-				else
-					System.out.print("* ");
+		System.out.println();
+		for (int i = 0; i < aux.length; i++) {
+			for (int j = 0; j < aux.length; j++) {
+				System.out.print(aux[i][j] + " ");
 			}
 			System.out.println();
 		}
-		System.out.println();
 	}
-
 }
