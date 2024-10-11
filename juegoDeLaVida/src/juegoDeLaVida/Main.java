6e71c1e421e94d3080af271db551ae164946f196
Unai <unai.carbajo.amundarain@gmail.com>
2024-10-11 16:56:04 +0200

    Comentarios

--- juegoDeLaVida/src/juegoDeLaVida/Main.java
+++ juegoDeLaVida/src/juegoDeLaVida/Main.java
@@ -3,57 +3,98 @@ package juegoDeLaVida;
 public class Main {
 
 	public static void main(String[] args) {
-		int[][] tabla = { { 0, 1, 1, 1, 0, 0 }, 
-					      { 1, 1, 0, 1, 1, 0,}, 
-					      { 1, 1, 0, 0, 1, 0 }, 
-					      { 0, 0, 1, 1, 0, 0 },
-					      { 0, 0, 0, 0, 0, 0 }, 
-					      { 0, 0, 0, 0, 0, 0 } };
-
-		int[][] aux = new int[tabla.length][tabla.length];
+        // Creamos una matriz de 0 y 1 para representar las celucas vivas(1) y las muertas (0).
+		int[][] tabla = { { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+                          { 0, 0, 0, 1, 1, 0, 0, 0, 0, 0 }, 
+                          { 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 },
+                          { 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 }, 
+                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, 
+                          { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }, };
 		
-		for (int i = 0; i < tabla.length; i++) {
-			for (int j = 0; j < tabla.length; j++) {
-				if (tabla[i][j] == 0)
-					System.out.print(". ");
-				else 
-					System.out.print("* ");
-			}
-			System.out.println();
-		}
-		
-		for (int fila = 0; fila < tabla.length; fila++) {
-			for (int columna = 0; columna < tabla.length; columna++) {
-				int suma = 0;
-				
-				for (int filaI = -1; filaI <= 1; filaI++) {
-					for (int columnaJ = -1; columnaJ <= 1; columnaJ++) {
-						if ((fila+filaI>=0 && fila+filaI<6) && (columna+columnaJ>=0 && columna+columnaJ<6))
-	                        suma += tabla[fila + filaI][columna + columnaJ];
+		// Creamos una matriz con la longitud de la primera matriz en la que guardaremos el resultado de cada celda.
+        int[][] aux = new int[tabla.length][tabla.length];
+        
+        // Mostramos la tabla inicial, para poder el estado inicial de las celulas.
+        System.out.println("Salida inicial");
+        for (int i = 0; i < tabla.length; i++) {
+            for (int j = 0; j < tabla.length; j++) {
+                //Cambiamos los 0 por . y los 1 con * para poder verlos mejor en la consola.
+            	if (tabla[i][j] == 0)
+                    System.out.print(". ");
+                else
+                    System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+        // En este primer for(), indicamos cuantas veces queremos repetir el codigo que esta debajo. En este caso es 10.
+        for (int repeticion = 0; repeticion < 10; repeticion++) {
+        	//Hacemos 2 for() para movernos por cada una de las celdas de la matriz.
+            for (int fila = 0; fila < tabla.length; fila++) {
+                for (int columna = 0; columna < tabla.length; columna++) {
+                    
+                	//Creamos una variable que sera la suma de los vecinos de cada matriz por la que avance.
+                	int suma = 0;
+                	
+                	//Creamos 2 for para movernos alrededor de las celdas de la celda activa. Al tener que alrededor de la celda actual,
+                	//los for() tendran 3 movimientos por fila y columna, se mueven entre el -1/0/1.
+                    for (int filaI = -1; filaI <= 1; filaI++) {
+                        for (int columnaJ = -1; columnaJ <= 1; columnaJ++) {
+                        	//Solucion al problema de que nos salgamos de la matriz, ya que la matriz no acepta numeros negativos.
+                        	//Si el if() sale false, ignora la celda no existente y continua. Si el valor es true, se suma a la variable
+                        	//suma el valor de la celda y lo acumula, dandondos el total de vecinos que tiene la celda activa.
+                            if ((fila + filaI >= 0 && fila + filaI < 10)
+                                    && (columna + columnaJ >= 0 && columna + columnaJ < 10))
+                                suma += tabla[fila + filaI][columna + columnaJ];
 
-					}
-				}
-				suma -= tabla[fila][columna];
-				
-				if ((tabla[fila][columna] == 1 || tabla[fila][columna] == 0 ) && (suma < 2 || suma > 3)) {
-					aux[fila][columna] = 0;
-				}else if ((tabla[fila][columna] == 1) && (suma == 2 || suma == 3)) {
-					aux[fila][columna] = 1;
-				}else if (suma == 3)
-					aux[fila][columna] = 1; {
-					
-				} 
-				}
-			}
-		System.out.println();
-		for (int i = 0; i < aux.length; i++) {
-			for (int j = 0; j < aux.length; j++) {
-				if (aux[i][j] == 0)
-					System.out.print(". ");
-				else 
-					System.out.print("* ");
-			}
-			System.out.println();
-		}
-	}
+                        }
+                    }
+                    //Como hemos sumado todas las celdas, incluida la celda activa, tenemos que restarle a la variable suma, 
+                    //el contenido de la celda activa
+                    suma -= tabla[fila][columna];
+                    
+                    //Los siguiente son las reglas del juego.
+                    	//Esto indica que si la celda activa esta viva y el resultado de sus vecinos es que tiene menos de 2 o mas de 3
+                    	//muere y se combierte en una celula muerta
+                    if ((tabla[fila][columna] == 1) && (suma < 2 || suma > 3)) {
+                        aux[fila][columna] = 0;
+                        //Esto indica que que si la celda activa esta viva y tiene entre 2 y 3 vecinos, la celula sigue viva.
+                    } else if ((tabla[fila][columna] == 1) && (suma == 2 || suma == 3)) {
+                        aux[fila][columna] = 1;
+                        //esto indica que si una celda tiene justo 3 vecinos la celula muerta, vive.
+                    } else if (suma == 3)
+                        aux[fila][columna] = 1;
+                    {
+
+                    }
+                }
+            }
+            
+            //Creamos un espacio entre la matriz que hemos mostrado y la siguiente matriz
+            System.out.println();
+            //Creacion de las generaciones. Mostramos como quedan las siguientes matrices
+            System.out.println("Generacion " + (repeticion+1));
+            for (int i = 0; i < aux.length; i++) {
+                for (int j = 0; j < aux.length; j++) {
+                    if (aux[i][j] == 0)
+                        System.out.print(". ");
+                    else
+                        System.out.print("* ");
+                }
+                System.out.println();
+            }
+            //Copiamos al final el estado de la matriz aux a la matriz tabla, para que en la repeticion del codigo quede reflejado en la matriz
+            //tabla el estado actualizado del resultado y asi en la siguiente repeticion del codigo no repetira el estado de la tabla inicial,
+            //si no de la generacion que hemos conseguido.
+            for (int k = 0; k < tabla.length; k++) {
+                for (int l = 0; l < tabla.length; l++) {
+                    tabla[k][l] = aux[k][l];
+                }
+            }
+        }
+    }
 }
